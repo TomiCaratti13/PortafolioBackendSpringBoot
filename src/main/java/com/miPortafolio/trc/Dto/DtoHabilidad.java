5ae86e426ecfa6c95569aeadea37ad89ab9c5ec7
Tomás Caratti <112879733+TomiCaratti13@users.noreply.github.com>
2023-05-13 01:39:35 -0300

    Update DtoHabilidad.java

--- src/main/java/com/miPortafolio/trc/Dto/DtoHabilidad.java
+++ src/main/java/com/miPortafolio/trc/Dto/DtoHabilidad.java
@@ -8,13 +8,16 @@ public class DtoHabilidad {
     @NotBlank
     private String nombreHys;
     @NotBlank
+    private String icono;
+    @NotBlank
     private int porcentaje;
 
     public DtoHabilidad() {
     }    
 
-    public DtoHabilidad(String nombreHys, int porcentaje) {
+    public DtoHabilidad(String nombreHys, String icono, int porcentaje) {
         this.nombreHys = nombreHys;
+        this.icono = icono;
         this.porcentaje = porcentaje;
     }
 
@@ -24,6 +27,14 @@ public class DtoHabilidad {
 
     public void setNombreHys(String nombreHys) {
         this.nombreHys = nombreHys;
+    }
+    
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
     }
 
     public int getPorcentaje() {
